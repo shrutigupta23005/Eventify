ad2d8cfa247df4788c652d29257f31ef8736b39a
Muskan gupta <shrutigupta23005@gmail.com>
2026-09-23 01:26:11 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,10 +1,25 @@
+# import mysql.connector
+
+# def get_db_connection():
+#     conn = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="",     # 👈 leave empty (since Workbench doesn't ask)
+#         database="university_chatbot"
+#     )
+#     return conn
+
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_connection():
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",     # 👈 leave empty (since Workbench doesn't ask)
-        database="university_chatbot"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     return conn
